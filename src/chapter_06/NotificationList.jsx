d4aebbce50423ff7a05fd5f35afc1277e5c04368
bisper <bisper@outlook.kr>
2022-07-01 03:38:39 +0900

    NotificationList 컴포넌트 수정.

--- src/chapter_06/NotificationList.jsx
+++ src/chapter_06/NotificationList.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import Notification from "./Notification";
 
 const reservedNotifications = [
-  {message: "안녕하세요, 오늘 일정을 알려드립니다."},
-  {message: "점심 식사 시간입니다."},
-  {message: "이제 곧 미팅이 시작됩니다."},
+  {
+    id: 1,
+    message: "안녕하세요, 오늘 일정을 알려드립니다."
+  },
+  {
+    id: 2,
+    message: "점심 식사 시간입니다."
+  },
+  {
+    id:3,
+    message: "이제 곧 미팅이 시작됩니다."},
 ];
 
 let timer;
@@ -29,6 +37,9 @@ class NotificationList extends React.Component {
         });
       }
       else {
+        this.setState({
+          notifications: [],
+        });
         clearInterval(timer);
       }
     }, 1000);
@@ -38,7 +49,12 @@ class NotificationList extends React.Component {
     return (
       <div>
         {this.state.notifications.map((notification) => {
-          return <Notification message={notification.message} />;
+          return (
+            <Notification
+              id={notification.id}
+              key={notification.id}
+              message={notification.message} />
+          );
         })}
       </div>
     );
