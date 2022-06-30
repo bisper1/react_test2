e6b828f764b098a9fbd8b3ac27b5332cf5c765a3
lecture-bisper <bisper@outlook.kr>
2022-06-30 16:40:40 +0900

    NotificationList 컴포넌트 추가.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import Clock from "./chapter_04/Clock"
 import Welcome from "./chapter_04/Welcome";
 import CommentList from "./chapter_05/CommentList";
+import NotificationList from "./chapter_06/NotificationList"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,6 +15,7 @@ root.render(
     {/* <App /> */}
     {/*<Library />*/}
     <CommentList />
+    <NotificationList />
   </React.StrictMode>
 );
 
