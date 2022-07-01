965fe869e7ebf328205a7415ebcbdec1de3e2114
lecture-bisper <bisper@outlook.kr>
2022-07-01 16:13:58 +0900

    map() 사용 시 key를 사용하여 고유한 값 설정하기.

--- src/chapter_05/CommentList.jsx
+++ src/chapter_05/CommentList.jsx
@@ -3,14 +3,17 @@ import Comment from "./Comment"
 
 const comments = [
   {
+    id: 1,
     name: "이인제",
     comment: "안녕하세요, 소플입니다.",
   },
   {
+    id: 2,
     name: "유재석",
     comment: "리액트 재미있어요~!",
   },
   {
+    id: 3,
     name: "강민경",
     comment: "저도 리액트 배워보고 싶어요!",
   },
@@ -21,7 +24,7 @@ function CommentList(props) {
     <div>
       {comments.map((comment) => {
         return (
-          <Comment name={comment.name} comment={comment.comment} />
+          <Comment key={comment.id} name={comment.name} comment={comment.comment} />
         );
       })}
     </div>
