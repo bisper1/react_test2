f75902ef739ac8ad58190f586ebed4c4ee62aeb9
lecture-bisper <bisper@outlook.kr>
2022-07-01 11:17:50 +0900

    useState, useEffect 사용하기.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Clock from "./chapter_04/Clock"
 import Welcome from "./chapter_04/Welcome";
 import CommentList from "./chapter_05/CommentList";
 import NotificationList from "./chapter_06/NotificationList"
+import Accommodate from "./chapter_07/Accommodate";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -16,6 +17,7 @@ root.render(
     {/*<Library />*/}
     <CommentList />
     <NotificationList />
+    <Accommodate />
   </React.StrictMode>
 );
 
